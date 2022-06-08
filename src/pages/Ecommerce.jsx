245f02adabda9832b5b6adb837f82db9ad141518
yassin mohamed <yassin_mohamed310@outlook.com>
2022-06-08 13:27:33 +0200

    create revenue section

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -30,14 +30,14 @@ const Ecommerce = () => {
                 </div>
 
                 <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-                    {earningData.map((item)=>(
-                        <div 
+                    {earningData.map((item) => (
+                        <div
                             className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg 
                                         md:w-56 p-4 pt-9 rounded-2xl"
                             key={item.title}>
-                            <button type="button" style={{color:item.iconColor , backgroundColor:item.iconBg}}
+                            <button type="button" style={{ color: item.iconColor, backgroundColor: item.iconBg }}
                                 className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
-                                >
+                            >
                                 {item.icon}
                             </button>
                             <p className="mt-3">
@@ -53,7 +53,61 @@ const Ecommerce = () => {
                     ))}
                 </div>
             </div>
-            
+
+            <div className="flex gap-10 flex-wrap justify-center ">
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+                    <div className="flex justify-between ">
+                        <p className="font-semibold text-xl">Revenue Updates</p>
+                        <div className="flex items-center gap-4">
+                            <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                                <span>
+                                    <GoPrimitiveDot />
+                                </span>
+                                <span>Expense</span>
+                            </p>
+                            <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                                <span>
+                                    <GoPrimitiveDot />
+                                </span>
+                                <span>Budget</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="mt-10 flex gap-10 flex-wrap justify-center">
+                        <div className="border-r-1 border-color m-4 pr-10">
+                            <div>
+                                <p>
+                                    <span className="text-3xl font-semibold">$98,154</span>
+                                    <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-sm">23%</span>
+                                </p>
+                                <p className="text-gray-500 mt-1">Budget</p>
+                            </div>
+                            <div className="mt-8">
+                                <p>
+                                    <span className="text-3xl font-semibold">$89,154</span>
+                                </p>
+                                <p className="text-gray-500 mt-1">Expense</p>
+                            </div>
+
+                            <div className="mt-5">
+                                <SparkLine 
+                                    currentColor="blue"
+                                    id="line-sparkline"
+                                    type="Line"
+                                    height="80px"
+                                    width="250px"
+                                    data={SparklineAreaData}
+                                    color="blue"
+                                />
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
